06082de434b443724f34236f4feab9fd549ec637
Akhilesh1902 <akhil1922002@gmail.com>
2023-05-10 20:44:31 +0530

    fixed some warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import { AnimatePresence } from 'framer-motion';
 import { Footer, Nav } from './Components';
 import Pages from './Pages';
-import { useLocation } from 'react-router-dom';
-import CanvasWrapper from './Components/ThreeJS/CanvasWrapper';
 import Modal from './Components/UI/Modal';
 import { useState } from 'react';
 
 function App() {
-  const location = useLocation();
-
   const [openModal, setOpenModel] = useState(false);
   const [modalType] = useState<'email'>('email');
   console.log(openModal);
   return (
     <>
       <AnimatePresence>
-        {location.pathname === '/solutions/store3d2' && (
-          <div className='h-screen w-screen bg-black overflow-x-cli fixed top-0 -z-10  '>
-            <CanvasWrapper />
-          </div>
-        )}
         <Nav />
         <div className='absolute z-0 w-screen'>
           <Pages />
